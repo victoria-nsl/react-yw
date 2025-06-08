42b6182ebd8a711fbf548080d34008648b85271e
victoria-nsl <vbnaish@yandex.ru>
2025-06-08 19:21:52 +0300

    feat(not-found): add content

--- src/pages/not-found/not-found.tsx
+++ src/pages/not-found/not-found.tsx
@@ -1,5 +1,16 @@
 import styles from './not-found.module.css';
+import error404 from '../../images/error-404.jpg';
+import { Link } from 'react-router-dom';
 
 export const NotFound = (): React.JSX.Element => {
-	return <div className={styles.wrapper}>Страница не найдена</div>;
+	return (
+		<div className={styles.wrapper}>
+			<div className={`${styles.wrapper_image} mb-10`}>
+				<img src={error404} alt='404 error.' />
+			</div>
+			<Link to='/' className={`${styles.link} text text_type_main-medium`}>
+				Перейти на главную страницу
+			</Link>
+		</div>
+	);
 };
