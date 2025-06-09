50243621e7dda27276ceb29f15e1116fc742c482
victoria-nsl <vbnaish@yandex.ru>
2025-06-09 20:53:12 +0300

    feat(app-header): add active link

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import styles from './app-header.module.css';
 import {
 	BurgerIcon,
@@ -12,24 +13,41 @@ export const AppHeader = () => {
 			<nav className={`${styles.menu} p-4`}>
 				<div className={styles.menu_part_left}>
 					{/*пока тут должны быть ссылки, а не например кнопки или абзацы*/}
-					<a href='/' className={`${styles.link} ${styles.link_active}`}>
-						<BurgerIcon type='primary' />
-						<p className='text text_type_main-default ml-2'>Конструктор</p>
-					</a>
-					<a href='/feed' className={`${styles.link} ml-10`}>
-						<ListIcon type='secondary' />
-						<p className='text text_type_main-default ml-2'>Лента заказов</p>
-					</a>
+					<NavLink to='/' className={styles.link}>
+						{({ isActive }) => (
+							<div
+								className={`${styles.link_content} ${isActive ? styles.link_content_active : ''}`}>
+								<BurgerIcon type='primary' />
+								<p className='text text_type_main-default ml-2'>Конструктор</p>
+							</div>
+						)}
+					</NavLink>
+					<NavLink to='/feed' className={`${styles.link} ml-10`}>
+						{({ isActive }) => (
+							<div
+								className={`${styles.link_content} ${isActive ? styles.link_content_active : ''}`}>
+								<ListIcon type='secondary' />
+								<p className='text text_type_main-default ml-2'>
+									Лента заказов
+								</p>
+							</div>
+						)}
+					</NavLink>
 				</div>
 				<div className={styles.logo}>
 					<Logo />
 				</div>
-				<a
-					href='/profile'
-					className={`${styles.link} ${styles.link_position_last}`}>
-					<ProfileIcon type='secondary' />
-					<p className='text text_type_main-default ml-2'>Личный кабинет</p>
-				</a>
+				<NavLink
+					to='/profile'
+					className={`${styles.link}  ${styles.link_position_last}`}>
+					{({ isActive }) => (
+						<div
+							className={`${styles.link_content} ${isActive ? styles.link_content_active : ''}`}>
+							<ProfileIcon type='secondary' />
+							<p className='text text_type_main-default ml-2'>Личный кабинет</p>
+						</div>
+					)}
+				</NavLink>
 			</nav>
 		</header>
 	);
