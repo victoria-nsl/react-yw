0057355c95508ad5ed5103a434f0115454189017
victoria-nsl <vbnaish@yandex.ru>
2025-06-10 07:39:10 +0300

    refactor(app-header): change links

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -13,25 +13,23 @@ export const AppHeader = () => {
 			<nav className={`${styles.menu} p-4`}>
 				<div className={styles.menu_part_left}>
 					{/*пока тут должны быть ссылки, а не например кнопки или абзацы*/}
-					<NavLink to='/' className={styles.link}>
-						{({ isActive }) => (
-							<div
-								className={`${styles.link_content} ${isActive ? styles.link_content_active : ''}`}>
-								<BurgerIcon type='primary' />
-								<p className='text text_type_main-default ml-2'>Конструктор</p>
-							</div>
-						)}
+					<NavLink
+						to='/'
+						className={({ isActive }) =>
+							isActive ? styles.link_active + ' ' + styles.link : styles.link
+						}>
+						<BurgerIcon type='primary' />
+						<p className='text text_type_main-default ml-2'>Конструктор</p>
 					</NavLink>
-					<NavLink to='/feed' className={`${styles.link} ml-10`}>
-						{({ isActive }) => (
-							<div
-								className={`${styles.link_content} ${isActive ? styles.link_content_active : ''}`}>
-								<ListIcon type='secondary' />
-								<p className='text text_type_main-default ml-2'>
-									Лента заказов
-								</p>
-							</div>
-						)}
+					<NavLink
+						to='/feed'
+						className={({ isActive }) =>
+							isActive
+								? styles.link_active + ' ' + styles.link + ' ml-10'
+								: styles.link + ' ml-10'
+						}>
+						<ListIcon type='secondary' />
+						<p className='text text_type_main-default ml-2'>Лента заказов</p>
 					</NavLink>
 				</div>
 				<div className={styles.logo}>
@@ -39,14 +37,18 @@ export const AppHeader = () => {
 				</div>
 				<NavLink
 					to='/profile'
-					className={`${styles.link}  ${styles.link_position_last}`}>
-					{({ isActive }) => (
-						<div
-							className={`${styles.link_content} ${isActive ? styles.link_content_active : ''}`}>
-							<ProfileIcon type='secondary' />
-							<p className='text text_type_main-default ml-2'>Личный кабинет</p>
-						</div>
-					)}
+					className={({ isActive }) =>
+						isActive
+							? styles.link_active +
+								' ' +
+								styles.link +
+								' ' +
+								styles.link_position_last +
+								' ml-10'
+							: styles.link + ' ' + styles.link_position_last + ' ml-10'
+					}>
+					<ProfileIcon type='secondary' />
+					<p className='text text_type_main-default ml-2'>Личный кабинет</p>
 				</NavLink>
 			</nav>
 		</header>
