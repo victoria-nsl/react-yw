e227311826f8bbe63843a0d8451130e4169552ec
victoria-nsl <vbnaish@yandex.ru>
2025-07-26 17:18:18 +0300

    feat: tests for auth

---
+++ src/services/auth/reducer.test.js
@@ -0,0 +1,51 @@
+import { describe, it, expect } from 'vitest';
+import { authReducer, initialState } from './reducer';
+import * as types from './actions';
+
+const user = {
+	success: true,
+	user: {
+		email: 'vvvv@yandex.ru',
+		name: 'Виктория',
+	},
+	accessToken:
+		'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4ODRlMWMzZDVjYTMwMDAxY2ZmYzIyOSIsImlhdCI6MTc1MzUzOTAxMSwiZXhwIjoxNzUzNTQwMjExfQ.h81Xg61baAF1nsxhbCR0mB5dUZ-ykGmFbecNfLVFqbU',
+	refreshToken:
+		'b547aa570156668e314abfd52de25fb1e3bd757715edf82615f1763625246a4bd50df40bfeabc831',
+};
+
+describe('auth reducer', () => {
+	it('correct initialization', () => {
+		const state = authReducer(undefined, { type: '' });
+
+		expect(state).toEqual(initialState);
+	});
+
+	it('set user', () => {
+		const action = {
+			type: types.SET_USER,
+			payload: user,
+		};
+
+		const state = authReducer(initialState, action);
+
+		expect(state).toEqual({
+			...initialState,
+			user,
+		});
+	});
+
+	it('check user auth', () => {
+		const action = {
+			type: types.SET_IS_AUTH_CHECKED,
+			payload: true,
+		};
+
+		const state = authReducer(initialState, action);
+
+		expect(state).toEqual({
+			...initialState,
+			isAuthChecked: true,
+		});
+	});
+});
