d026d01018fd259d864889872029736b34101e48
victoria-nsl <vbnaish@yandex.ru>
2025-07-27 20:41:17 +0300

    feat: tests for modal ingredient

---
+++ cypress/e2e/modal-ingredient.cy.ts
@@ -0,0 +1,68 @@
+/// <reference types="cypress" />
+
+describe('drag and drop ingredients into the constructor', () => {
+	beforeEach(() => {
+		cy.intercept('GET', 'api/auth/user', { fixture: 'user.json' });
+		cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients.json' });
+
+		cy.visit('http://localhost:5173/');
+	});
+
+	it('opening a modal window with a description of the ingredient', () => {
+		cy.get('[data-testid=drag_ingredient]')
+			.contains('Хрустящие минеральные кольца')
+			.as('ingredientRings');
+
+		cy.get('@ingredientRings').click();
+
+		cy.get('[data-testid=modal]').as('modal');
+
+		cy.get('@modal').should('exist');
+		cy.get('@modal').contains('Детали ингредиента');
+		cy.get('@modal').contains('Хрустящие минеральные кольца');
+	});
+
+	it('closing the modal window with the ingredient description by clicking on the cross', () => {
+		cy.get('[data-testid=drag_ingredient]')
+			.contains('Хрустящие минеральные кольца')
+			.as('ingredientRings');
+
+		cy.get('@ingredientRings').click();
+
+		cy.get('[data-testid=modal]').as('modal');
+
+		cy.get('@modal').find('button').as('button');
+		cy.get('@button').click();
+
+		cy.get('@modal').should('not.exist');
+	});
+
+	it('closing the modal window with the ingredient description by clicking on the overlay', () => {
+		cy.get('[data-testid=drag_ingredient]')
+			.contains('Соус традиционный галактический')
+			.as('ingredientSauce');
+
+		cy.get('@ingredientSauce').click();
+
+		cy.get('[data-testid=modal]').as('modal');
+		cy.get('[data-testid=modal-overlay]').as('modalOverlay');
+
+		cy.get('@modalOverlay').click({ force: true });
+
+		cy.get('@modal').should('not.exist');
+	});
+
+	it('close modal window with ingredient description when pressing key Esc', () => {
+		cy.get('[data-testid=drag_ingredient]')
+			.contains('Соус традиционный галактический')
+			.as('ingredientSauce');
+
+		cy.get('@ingredientSauce').click();
+
+		cy.get('[data-testid=modal]').as('modal');
+
+		cy.get('body').type('{esc}');
+
+		cy.get('@modal').should('not.exist');
+	});
+});
